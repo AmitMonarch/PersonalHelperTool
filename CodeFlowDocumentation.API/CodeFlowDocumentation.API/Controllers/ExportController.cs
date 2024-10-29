da3b994e71fb28b70493e8bc663fb42d638fa070
Amitesh Kumar <amitesh.kumar@safesend.com>
2024-10-29 19:55:03 +0530

    Initial Commit

---
+++ CodeFlowDocumentation.API/CodeFlowDocumentation.API/Controllers/ExportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeFlowDocumentation.API.Controllers
+{
+    public class ExportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CodeFlowDocumentation.API/CodeFlowDocumentation.API/Controllers/etc/HealthCheckController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient; // Updated to use Microsoft.Data.SqlClient
+using Microsoft.Extensions.Configuration;
+
+namespace CodeFlowDocumentation.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public HealthCheckController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult CheckDatabaseConnection()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    return Ok("Database connection is successful.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Database connection failed: {ex.Message}");
+            }
+        }
+    }
+}
